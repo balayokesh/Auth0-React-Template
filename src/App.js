f8486d3b4f0a0a8a5593f1ff456bf7b8508b45d6
balayokesh <balayokeshmani@gmail.com>
2021-11-09 12:17:54 +0530

    implemented basic login, logout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
+
+import Login from './Login';
+import Logout from './Logout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Link to='/login' component={Login}>Login</Link>
+      </div>
+      <div>
+        <Link to='/logout' component={Logout}>Logout</Link>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import App from "./App";
+import Login from "./Login";
+import Logout from "./Logout";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="balayokesh.eu.auth0.com"
+    clientId="eDVL9dzVfiyBO5epWCA7VtoCgyZswzrU"
+    redirectUri={window.location.origin}
+  >
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/logout' element={<Logout/>} />
+      </Routes>
+    </BrowserRouter>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

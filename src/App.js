1b5793def8cdfb87fbd9b961ca562da801b700aa
balayokesh <balayokeshmani@gmail.com>
2021-11-09 15:52:50 +0530

    Implemented getting user information functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import { Link } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 
 import Login from './Login';
 import Logout from './Logout'
 
 function App() {
-  return (
-    <>
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  if (isAuthenticated) {
+    return (
+      <>
+        <div>
+          <Link to='/logout' component={Logout}>Logout</Link>
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+  else {
+    return (
       <div>
         <Link to='/login' component={Login}>Login</Link>
       </div>
-      <div>
-        <Link to='/logout' component={Logout}>Logout</Link>
-      </div>
-    </>
-  );
+    )
+  }
 }
 
 export default App;

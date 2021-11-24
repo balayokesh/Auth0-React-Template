ab6740afc6f46f67b3ba9e913625e9425de38658
balayokesh <balayokeshmani@gmail.com>
2021-11-24 14:08:04 +0530

    Minify code, deleted unwanted files, routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import { Link } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 
-import Login from './Login';
-import Logout from './Logout'
+export default function App() {
+	const { user, isAuthenticated, isLoading } = useAuth0();
+	const { loginWithRedirect } = useAuth0();
+	const { logout } = useAuth0();
 
-function App() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-  if (isAuthenticated) {
-    if (isLoading) {
-      return <div>Loading...</div>
-    }
-    return (
-      <>
-        <div>
-          <Link to='/logout' component={Logout}>Logout</Link>
-          <div>
-            <img src={user.picture} alt={user.name} />
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-          </div>
-        </div>
-      </>
-    );
-  }
-  else {
-    return (
-      <div>
-        <Link to='/login' component={Login}>Login</Link>
-      </div>
-    )
-  }
+	if (isLoading) {
+		return <div>Loading...</div>
+	}
+    if (isAuthenticated) {
+		return (
+			<>
+				<div>
+					<button onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
+					<div>
+						<img src={user.picture} alt={user.name} />
+						<h2>{user.name}</h2>
+						<p>{user.email}</p>
+					</div>
+				</div>
+			</>
+		);
+	}
+	else {
+		return (
+			<div>
+				<button onClick={loginWithRedirect}>Login</button>
+			</div>
+		)
+	}
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,25 +4,21 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App";
-import Login from "./Login";
-import Logout from "./Logout";
 
 const Routing = () => {
-  return (
-    <Auth0Provider
-    domain="balayokesh.eu.auth0.com"
-    clientId="eDVL9dzVfiyBO5epWCA7VtoCgyZswzrU"
-    redirectUri={window.location.origin}
-  >
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-      <Route path='/' exact element={<App/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/logout' element={<Logout/>} />
-      </Routes>
-    </BrowserRouter>
-  </Auth0Provider>
-  )
+	return (
+		<Auth0Provider
+			domain="balayokesh.eu.auth0.com"
+			clientId="eDVL9dzVfiyBO5epWCA7VtoCgyZswzrU"
+			redirectUri={window.location.origin}
+		>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' exact element={<App />} />
+				</Routes>
+			</BrowserRouter>
+		</Auth0Provider>
+	)
 }
 
 ReactDOM.render(<Routing />, document.getElementById("root"));
